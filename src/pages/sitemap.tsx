6c750cc2b5f164952bd3981da834593a69af36f2
Antony Holmes <antony@antonybholmes.com>
2021-07-02 22:22:44 -0400

    Bug fixes and updates.

--- src/pages/sitemap.tsx
+++ src/pages/sitemap.tsx
@@ -27,23 +27,21 @@ const SiteMapPage: React.FC<PageProps> = ({ path }) => {
               <div className="mt-2">
                 <BlueLink to="/terms">Terms of Use</BlueLink>
               </div>
-
-              <div className="mt-2">
-                <BlueLink to="/contact">Contact Us</BlueLink>
-              </div>
             </div>
 
             <div className="w-full md:w-1/3">
               <h4 className="mt-4">Learn More</h4>
-              {headerLinks.map((link: {name:string, url:string}, i: number) => {
-                return (
-                  <div className="mt-2" key={i}>
-                    <BlueLink aria-label={`Goto ${link.name}`} to={link.url}>
-                      {link.name}
-                    </BlueLink>
-                  </div>
-                )
-              })}
+              {headerLinks.map(
+                (link: { name: string; url: string }, i: number) => {
+                  return (
+                    <div className="mt-2" key={i}>
+                      <BlueLink aria-label={`Goto ${link.name}`} to={link.url}>
+                        {link.name}
+                      </BlueLink>
+                    </div>
+                  )
+                }
+              )}
             </div>
           </Row>
         </div>
